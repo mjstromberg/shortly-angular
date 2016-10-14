e3bd703106ec9bfc9f73b1563a2ecdfcb1c14194
mjstromberg <michaeljstromberg@gmail.com>
2016-10-14 16:06:56 -0700

    Add navigation

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,6 +1,7 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
+  $location.path('/signin');
   $scope.link = {};
   $scope.addLink = function(link) {
     Links.addOne(link);
