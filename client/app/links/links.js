35732a95b407b1c646c8c3ec747735847d870f5a
mjstromberg <michaeljstromberg@gmail.com>
2016-10-15 10:53:23 -0700

    Implement sorting links by visits and search bar

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,9 +3,22 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links) {
 
   $scope.data = {};
+  $scope.urlSearchString = '';
   
   Links.getAll().then(function (links) {
+    links.sort(function(a, b) {
+      return a.visits < b.visits;
+    });
     $scope.data.links = links;
   });
+
+  $scope.filterLinks = function() {
+    if ($scope.data.links) {
+      return $scope.data.links.filter(function(link) {
+        var regex = new RegExp($scope.urlSearchString);
+        return regex.test(link.url);
+      });
+    }
+  };
 
 });
